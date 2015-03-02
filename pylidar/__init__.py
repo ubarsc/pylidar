ee9bdea0150ef18296403ddfe8773ee7b81e7b19
Sam Gillingham <gillingham.sam@gmail.com>
2015-03-02 15:40:18 +1000

    license headers

--- pylidar/__init__.py
+++ pylidar/__init__.py
@@ -1,4 +1,22 @@
 
 """
+Main module. See the lidarprocessing sub module for the
+doProcessing() function and other useful things.
 """
+# This file is part of PyLidar
+# Copyright (C) 2015 John Armston, Neil Flood and Sam Gillingham
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 PYLIDAR_VERSION = '0.01'
--- pylidar/lidarformats/spdv3.py
+++ pylidar/lidarformats/spdv3.py
@@ -1,7 +1,23 @@
 
 """
-SPD V3
+SPD V3 format driver and support functions
 """
+# This file is part of PyLidar
+# Copyright (C) 2015 John Armston, Neil Flood and Sam Gillingham
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import copy
 import numpy
 import h5py
