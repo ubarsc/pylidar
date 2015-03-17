f56c721281174c2684731a408d0062bf6c855b40
Neil Flood <Neil.Flood@derm.qld.gov.au>
2015-03-17 14:50:20 +1000

    Avoid using numpy.full(), since I do not yet have the latest version of numpy installed. This is equivalent.

--- tests/testWriteImageFromPoints.py
+++ tests/testWriteImageFromPoints.py
@@ -18,7 +18,8 @@ def writeImageFunc(data):
         minZ = zValues.min(axis=0)
         stack = numpy.ma.expand_dims(minZ, axis=0)
     else:
-        stack = numpy.full((1, nRows, nCols), nullval, dtype=zValues.dtype)
+        stack = numpy.empty((1, nRows, nCols), dtype=zValues.dtype)
+        stack.fill(nullval)
     data.imageOut1.setData(stack)
     
     
