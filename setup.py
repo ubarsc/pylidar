4def7d30779d6bf8f415307e6cbc3d1d30e9936f
Sam Gillingham <gillingham.sam@gmail.com>
2015-11-09 12:33:43 +1000

    now get extra compile flags from $PYLIDAR_CXX_FLAGS, if present

--- setup.py
+++ setup.py
@@ -25,29 +25,52 @@ from numpy.distutils.core import setup, Extension
 
 import pylidar
 
+def getExtraCXXFlags():
+    """
+    Looks at the $PYLIDAR_CXX_FLAGS environment variable.
+    If it exists, this function returns a list of flags
+    to be passed as the extra_compile_args argument to
+    the Extension constructor.
+    Otherwise None.
+    """
+    if 'PYLIDAR_CXX_FLAGS' in os.environ:
+        return os.environ['PYLIDAR_CXX_FLAGS'].split()
+    else:
+        return None
+
+def addRieglDriver(extModules, cxxFlags):
+    """
+    Decides if the Riegl driver is to be built. If so 
+    adds the Extension class to extModules.
+    """
+    if 'RIVLIB_ROOT' in os.environ and 'RIWAVELIB_ROOT' in os.environ:
+        print('Building Riegl Extension...')
+        rivlibRoot = os.environ['RIVLIB_ROOT']
+        riwavelibRoot = os.environ['RIWAVELIB_ROOT']
+        rieglModule = Extension(name='pylidar.lidarformats._riegl', 
+                sources=['src/riegl.cpp', 'src/pylidar.c'],
+                include_dirs=[os.path.join(rivlibRoot, 'include'),
+                                os.path.join(riwavelibRoot, 'include')],
+                extra_compile_args=cxxFlags,
+                libraries=['scanlib-mt', 'riboost_chrono-mt', 
+                     'riboost_date_time-mt', 'riboost_filesystem-mt', 
+                     'riboost_regex-mt', 'riboost_system-mt', 
+                     'riboost_thread-mt', 'wfmifc-mt'],
+                library_dirs=[os.path.join(rivlibRoot, 'lib'),
+                                os.path.join(riwavelibRoot, 'lib')])
+                 
+        extModules.append(rieglModule)
+    else:
+        print('Riegl Libraries not found.')
+        print('If installed set $RIVLIB_ROOT to the install location of RiVLib')
+        print('and $RIWAVELIB_ROOT to the install location of the waveform extraction library (riwavelib)')
+
+# get any C++ flags
+cxxFlags = getExtraCXXFlags()
 # work out if we need to build any of the C/C++ extension
 # modules
 externalModules = []
-if 'RIVLIB_ROOT' in os.environ and 'RIWAVELIB_ROOT' in os.environ:
-    print('Building Riegl Extension...')
-    rivlibRoot = os.environ['RIVLIB_ROOT']
-    riwavelibRoot = os.environ['RIWAVELIB_ROOT']
-    rieglModule = Extension(name='pylidar.lidarformats._riegl', 
-            sources=['src/riegl.cpp', 'src/pylidar.c'],
-            include_dirs=[os.path.join(rivlibRoot, 'include'),
-                            os.path.join(riwavelibRoot, 'include')],
-            extra_compile_args=['-std=c++0x'], # needed on my old version of Intel, not sure if it will cause problems elsewere
-            libraries=['scanlib-mt', 'riboost_chrono-mt', 'riboost_date_time-mt',
-                 'riboost_filesystem-mt', 'riboost_regex-mt', 
-                 'riboost_system-mt', 'riboost_thread-mt', 'wfmifc-mt'],
-            library_dirs=[os.path.join(rivlibRoot, 'lib'),
-                            os.path.join(riwavelibRoot, 'lib')])
-                 
-    externalModules.append(rieglModule)
-else:
-    print('Riegl Libraries not found.')
-    print('If installed set $RIVLIB_ROOT to the install location of RiVLib')
-    print('and $RIWAVELIB_ROOT to the install location of the waveform extraction library (riwavelib)')
+addRieglDriver(externalModules, cxxFlags)
 
 setup(name='pylidar',
       version=pylidar.PYLIDAR_VERSION,
