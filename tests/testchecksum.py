0ecf8d92e21b6525499e18f2cd783ed93e6bf402
Sam Gillingham <gillingham.sam@gmail.com>
2016-06-23 16:50:49 +1000

    remove 'tests' subdirectory

--- tests/testchecksum.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from __future__ import print_function, division
-
-import sys
-from pylidar.testing import utils
-
-cksum = utils.calculateCheckSum(sys.argv[1])
-print(cksum)
