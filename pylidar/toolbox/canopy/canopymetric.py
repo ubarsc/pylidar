5fd6a9bb0a3d235b9ded2f52d2c74840573056fd
John Armston <john.armston@dsitia.qld.gov.au>
2016-10-17 13:46:23 +1000

    Some progress on reading indexed spdv4 files to calculate pgap for pavd_calders2014 metric. Port of spdtools code incomplete and not for testing yet

--- pylidar/toolbox/canopy/canopymetric.py
+++ pylidar/toolbox/canopy/canopymetric.py
@@ -20,55 +20,56 @@ Module for calculating canopy metrics from lidar data.
 
 from __future__ import print_function, division
 
+import sys
 import numpy
 import importlib
+from pylidar.lidarformats import generic
 from pylidar import lidarprocessor
-from pylidar.toolbox.canopy import canopymetric
 from rios import cuiprogress
 
-DEFAULT_METRIC = "pavd_jupp2009"
+from pylidar.toolbox.canopy import pavd_calders2014
 
-
+DEFAULT_CANOPY_METRIC = "PAVD_CALDERS2014"
 
 
 class CanopyMetricError(Exception):
     "Exception type for canopymetric errors"
 
 
-def pavd_jupp2009(data, otherargs):
-	"""
-	Calculate PAVD using Jupp et al. (2009)
-	"""
+def pavd_calders2014(data, otherargs):
+    """
+    Calculate PAVD following Calders et al. (2014)
+    """
+    pointcolnames = ['Z','CLASSIFICATION','RETURN_NUMBER']
+    pulsecolnames = ['NUMBER_OF_RETURNS','ZENITH']   
     
-    ptValList = [indata.getPoints(colNames=['HEIGHT','CLASSIFICATION','RETURN_NUMBER']) for indata in data.inList]
-    ptValStack = numpy.hstack(ptValList)
+    pointsByBin = [indata.getPointsByBins(indexByPulse=True, returnPulseIndex=True, colNames=pointcolnames) for indata in data.inList]
+    pulsesByBin = [indata.getPulses(colNames=pulsecolnames, pulseIndex=pointsByBin[i][1]) for i,indata in enumerate(data.inList)]
     
+    pointsByBin = numpy.ma.vstack([p[0] for p in pointsByBin])
+    pulsesByBin = numpy.ma.vstack(pulsesByBin)
+    
+    (maxPts, nRows, nCols) = pointsByBin.shape  
     binCoords = data.info.getBlockCoordArrays()
     
-    print(binCoords)
+    
     sys.exit()
-	
-	
-def runCanopyMetric(infiles, outfile, binsize=None, metric=DEFAULT_METRIC, quiet=False):
+
+
+def runCanopyMetric(infiles, outfile, binsize=None, metric=DEFAULT_CANOPY_METRIC, quiet=False):
     """
     Apply canopy metric
     Metric name should be of the form <metric>_<source>
     """
+    
+    # Set all the input files
     dataFiles = lidarprocessor.DataFiles()
     dataFiles.inList = [lidarprocessor.LidarFile(fname, lidarprocessor.READ) 
                         for fname in infiles]
-    
-	otherArgs = lidarprocessor.OtherArgs()
-	
-	if metric == "pavd_jupp2009":
-		
-        otherArgs.heightbinsize = 0.5
         
-				
-	else:
-		msg = 'Unsupported metric %s' % metric
-        raise CanopyMetricError(msg)	
-	
+    #info = generic.getLidarFileInfo(infiles[0])
+    #print(info.header.keys())
+    
     controls = lidarprocessor.Controls()
     controls.setSpatialProcessing(True)
     if not quiet:
@@ -76,9 +77,32 @@ def runCanopyMetric(infiles, outfile, binsize=None, metric=DEFAULT_METRIC, quiet
         controls.setProgress(progress)
 
     if binsize is not None:
-        controls.setReferenceResolution(binsize)
-	
-	metricFunc = eval(metric)
-	
+        controls.setReferenceResolution(binsize)   
+    
+    otherArgs = lidarprocessor.OtherArgs()
+    
+    if metric == "PAVD_CALDERS2014":
+        
+        heightbinsize = 0.5
+        zenithbinsize = 5.0
+        
+        maxheight = 50.0
+        minzenith = 30.0
+        maxzenith = 70.0
+        
+        otherargs.zenith = numpy.arange(minzenith+zenithbinsize, maxzenith, zenithbinsize)
+        otherargs.height = numpy.arange(0, maxheight, heightbinsize)
+        otherargs.pgap = numpy.ones([otherargs.zenith.size,otherargs.height.size])
+        
+        controls.setWindowSize(64)
+        
+      
+    else:
+        msg = 'Unsupported metric %s' % metric
+        raise CanopyMetricError(msg)
+    
+    metricFunc = eval(metric.lower())
+    
     lidarprocessor.doProcessing(metricFunc, dataFiles, controls=controls, 
             otherArgs=otherArgs)
+            
