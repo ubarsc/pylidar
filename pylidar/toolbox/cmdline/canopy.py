89c4c809f26b39e91cfd9777b3297fc0ef379b23
John Armston <j.armston@uq.edu.au>
2017-06-15 18:46:03 -0400

    Removed reference to canopymetric.DEFAULT_CANOPY_METRIC as no longer exists

--- pylidar/toolbox/cmdline/canopy.py
+++ pylidar/toolbox/cmdline/canopy.py
@@ -35,7 +35,7 @@ def getCmdargs():
     p = argparse.ArgumentParser()
     p.add_argument("-i", "--infiles", nargs="+", help="Input lidar files (required)")
     p.add_argument("-o", "--output", nargs="+", help="Output file/s (required)")
-    p.add_argument("-m", "--metric", default=canopymetric.DEFAULT_CANOPY_METRIC, choices=['PAVD_CALDERS2014','VOXEL_HANCOCK2016','PGAP_ARMSTON2013'], 
+    p.add_argument("-m", "--metric", default="PAVD_CALDERS2014", choices=["PAVD_CALDERS2014","VOXEL_HANCOCK2016","PGAP_ARMSTON2013"], 
         help="Canopy metric to calculate. default=%(default)s")
     p.add_argument("-w","--weighted", default=False, action="store_true", help="Calculate Pgap(z) using weighted interception (Armston et al., 2013)")         
     p.add_argument("-p","--planecorrection", default=False, action="store_true", help="Apply plane correction to point heights (PAVD_CALDERS2014 metric only)")
